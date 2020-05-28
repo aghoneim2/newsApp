b0efce8acc2eaa0e9e088e3c3897ce0c0f2e0d6e
aghoneim2 <almalek154@gmail.com>
2020-05-28 15:48:20 -0400

    Update googleNews.py

--- googleNews.py
+++ googleNews.py
@@ -24,16 +24,32 @@ top_headlines = api.get_top_headlines(q='america',
 									language='en')
 
 
+print(top_headlines["totalResults"])
 print("Top Headlines")
 print("--------------------------------------------------")
 #print(top_headlines)
+list = []
+count = 0;
 for x in top_headlines["articles"]:
-	print("Title: ")
-	print(x["title"])
-	print("Description: ")
-	print(x["description"])#replaced the 'description' parameter with content, provides 
+	#print("Title: ")
+	title = (x["title"])
+	#print("Description: ")
+	description = (x["description"])#replaced the 'description' parameter with content, provides 
 						#info from article itself, but adds '[+xxx] characters' that must be truncated
 	print("---------------------------------------------------")
+	count = count+1;
+	tuple = (title, description)
+	list.append(tuple)
+
+print("Len of list: " , len(list))
+#if (count<20):
+
+for x in list:
+	print("Title: ")
+	print(x[0])
+	print("Description: ")
+	print(x[1])
+	print("---------------------------------------")
 
 
 #HOW IT IS CURRENTLY WORKING:
@@ -41,3 +57,8 @@ for x in top_headlines["articles"]:
 #query 'q'. i enter the location there for now to get all the articles related
 #to that search. all that remains is how to determine location in app.
 #and how i can send that location into the python file, from the app
+
+'''New Update 5/28/2020
+Currently take articles and store them in a list of tuples.
+I can access that information starting in line 47-52.
+'''
